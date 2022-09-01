5afb4df364e6462c7c576f022c3c97845d9efe62
Roberto Dias <roberto.rhd@gmail.com>
2022-09-01 16:43:40 -0300

    commit inicial

---
+++ README.md
@@ -0,0 +1,16 @@
+# Projeto cartão pessoal
+
+Um projeto para a criação de um cartão com informações pessoais.
+
+#
+
+# Font family para utilização
+
+- Family: [Kumbh Sans](https://fonts.google.com/specimen/Kumbh+Sans)
+- Weights: 400, 700
+
+# Linguagens
+
+- HTML
+- CSS
+- JS
---
+++ src/js/index.js
@@ -0,0 +1,31 @@
+const abas = document.querySelectorAll(".aba");
+const informacoes = document.querySelectorAll(".info");
+
+abas.forEach((aba) => {
+    aba.addEventListener("click", function () {
+
+        const abaClicadaEstaSelecionado = aba.classList.contains("selecionado");
+        if(abaClicadaEstaSelecionado) return;
+
+        const abaSelecionada = document.querySelector('.selecionado');
+        abaSelecionada.classList.remove('selecionado');
+
+        informacoes.forEach((info) => {
+            const estaSelecionada = info.classList.contains("selecionado");
+            if (estaSelecionada) {                
+                esconderInformacoes(info);
+            } else {
+                aba.classList.add('selecionado');
+                mostrarInformacoes(info);
+            }
+        });
+    });
+});
+
+function esconderInformacoes(info) {
+    info.classList.remove("selecionado");
+}
+
+function mostrarInformacoes(info) {
+    info.classList.add("selecionado");
+}
